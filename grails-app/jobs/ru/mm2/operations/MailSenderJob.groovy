ce76d7b10945f3cac90de62901bae30a50861ebb
Alex khitev <akhitev@localhost.localdomain>
2014-02-01 18:06:46 +0400

    https://github.com/alexkhitev/ru.mm2.operations/issues/23 https://github.com/alexkhitev/ru.mm2.operations/issues/21

--- grails-app/jobs/ru/mm2/operations/MailSenderJob.groovy
+++ grails-app/jobs/ru/mm2/operations/MailSenderJob.groovy
@@ -17,48 +17,70 @@ class MailSenderJob {
         DateFormat onlyTimeFormat = new SimpleDateFormat("HH:mm", new Locale("en","EN"))
         //Date date = dateFormat.parse(params.date_time);
         int prolong = 2
-        Date diapStart = new Date()+prolong-1
-        diapStart.hours=23
-        diapStart.minutes=59
-        Date diapFinish = new Date()+prolong+1
-        diapFinish.hours=0
-        diapFinish.minutes=0
+        Date diapStart = new Date()+prolong
+        diapStart.hours=0
+        diapStart.minutes=0
+        Date diapFinish = new Date()+prolong
+        diapFinish.hours=23
+        diapFinish.minutes=59
         println "${diapStart}"
         println "${diapFinish}"
         String mailText = "На ${onlyDateFormat.format(new Date()+prolong)}\r\n"
-        mailText = "${mailText} Список операций по ОМС:\r\n"
-        for (GenOperationRecord currOper : GenOperationRecord.findByDate_timeBetween(diapStart, diapFinish)){
-            mailText = "${mailText} ${currOper?.fio}, ${currOper?.operation?.name}, ${currOper?.diagnoz}, ${onlyDateFormat.format(currOper?.birthday)}\r\n"
+        mailText = "<html><head><meta http-equiv='Content-type' content='text/html; charset=utf-8' /><title></title></head><body><h3>На ${onlyDateFormat.format(new Date()+prolong)}</h3><br/><table border='1'>"
+        List<GenOperationRecord> genOperationRecordList = GenOperationRecord.findAllByDate_timeBetween(diapStart, diapFinish);
+        List<ComOperationRecord> comOperationRecordList = ComOperationRecord.findAllByDate_timeBetween(diapStart, diapFinish);
+        List<ConsultationRecord> consultationRecordList = ConsultationRecord.findAllByDate_timeBetween(diapStart, diapFinish);
+        List<ComConsultationRecord> comConsultationRecordList = ComConsultationRecord.findAllByDate_timeBetween(diapStart, diapFinish);
+        mailText = "${mailText} <tr style='background-color: #1E90FF; color: white'><td colspan='4'>Госпитализации по ОМС</td></tr><tr style='background-color: mediumpurple; color: white'><td colspan='2'>ФИО</td><td>Диагноз</td><td>Дата рождения</td></tr>"
+        if(genOperationRecordList.size()>0){
+            for (GenOperationRecord currOper : genOperationRecordList){
+                mailText = "${mailText}<tr><td colspan='2'>${currOper?.fio}</td><td>${currOper?.operation?.name}</td><td>${onlyDateFormat.format(currOper?.birthday)}</td></tr>"
+            }
+        }else{
+            mailText = "${mailText}<tr><td colspan='4'>Нет</td></tr>"
         }
-        mailText = "${mailText} Список операций по ДМС и ХОЗРАСЧЁТУ:\r\n"
-        for (ComOperationRecord currComOper : ComOperationRecord.findByDate_timeBetween(diapStart, diapFinish)){
-            mailText = "${mailText} ${currComOper?.fio}, ${currComOper?.operation?.name}, ${currComOper?.diagnoz}, ${onlyDateFormat.format(currComOper?.birthday)}\r\n"
+        mailText = "${mailText} <tr style='background-color: #1E90FF; color: white'><td colspan='4'>Госпитализации по ДМС и ХозРасчету</td></tr><tr style='background-color: forestgreen; color: white'><td colspan='2'>ФИО</td><td>Диагноз</td><td>Дата рождения</td></tr>"
+        if(comOperationRecordList.size()>0){
+            for (ComOperationRecord currComOper : comOperationRecordList){
+                mailText = "${mailText}<tr><td colspan='2'>${currComOper?.fio}</td><td>${currComOper?.operation?.name}</td><td>${onlyDateFormat.format(currComOper?.birthday)}</td></tr>"
+            }
+        }else{
+            mailText = "${mailText}<tr><td colspan='4'>Нет</td></tr>"
         }
-        mailText = "${mailText} Список консультаций по ОМС:\r\n"
-        for (ConsultationRecord currCons : ConsultationRecord.findByDate_timeBetween(diapStart, diapFinish)){
-            mailText = "${mailText} ${currCons?.fio}, ${onlyTimeFormat.format(currCons?.date_time)}, ${currCons?.diagnoz}, ${onlyDateFormat.format(currCons?.birthday)}\r\n"
+        mailText = "${mailText} <tr style='background-color: #1E90FF; color: white'><td colspan='4'>Консультации по ОМС</td></tr><tr style='background-color: mediumpurple; color: white'><td>Время</td><td>ФИО</td><td>Диагноз</td><td>Дата рождения</td></tr>"
+        if(consultationRecordList.size()>0){
+            for (ConsultationRecord currCons : consultationRecordList){
+                mailText = "${mailText}<tr><td>${currCons?.fio}</td><td>${onlyTimeFormat.format(currCons?.date_time)}</td><td>${currCons?.diagnoz}</td><td>${onlyDateFormat.format(currCons?.birthday)}</td></tr>"
+            }
+        }else{
+            mailText = "${mailText}<tr><td colspan='4'>Нет</td></tr>"
         }
-        mailText = "${mailText} Список консультаций по ДМС и ХОЗРАСЧЁТУ:\r\n"
-        for (ComConsultationRecord currComCons : ComConsultationRecord.findByDate_timeBetween(diapStart, diapFinish)){
-            mailText = "${mailText} ${currComCons?.fio}, ${onlyTimeFormat.format(currComCons?.date_time)}, ${currComCons?.diagnoz}, ${onlyDateFormat.format(currComCons?.birthday)}\r\n"
+        mailText = "${mailText} <tr style='background-color: #1E90FF; color: white'><td colspan='4'>Консультации по ДМС и ХозРасчету</td></tr><tr style='background-color: forestgreen; color: white'><td>Время</td><td>ФИО</td><td>Диагноз</td><td>Дата рождения</td></tr>"
+        if(comConsultationRecordList.size()>0){
+            for (ComConsultationRecord currComCons : comConsultationRecordList){
+                mailText = "${mailText}<tr><td>${currComCons?.fio}</td><td>${onlyTimeFormat.format(currComCons?.date_time)}</td><td>${currComCons?.diagnoz}</td><td>${onlyDateFormat.format(currComCons?.birthday)}</td></tr>"
+            }
+        }else{
+            mailText = "${mailText}<tr><td colspan='4'>Нет</td></tr>"
         }
-        /*mailService.sendMail{
+        mailText = "${mailText}</table></body></html>"
+        mailService.sendMail{
             from "ru.mm2.operations@gmail.com"
             to "art1977.kashin@gmail.com"
             subject "На ${new Date()+prolong}\r\n"
             text mailText
-        }*/
+        }
         mailService.sendMail{
             from "ru.mm2.operations@gmail.com"
             to "9045531637@mail.ru"
             subject "На ${new Date()+prolong}\r\n"
-            text mailText
+            html mailText
         }
         mailService.sendMail{
             from "ru.mm2.operations@gmail.com"
             to "alexkhitev@gmail.com"
             subject "На ${new Date()+prolong}\r\n"
-            text mailText
+            html mailText
         }
         println "!2"
     }
